30d9ad78e1f78ae5f16d234fc710e413c69a3512
erinadora <eringoncer@gmail.com>
2016-01-22 22:30:05 -0500

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,34 @@
-def addition(num1, num2)
+def addition(num1=5, num2=4)
+  puts "#{num1} + #{num2}"
+  return num1+num2
 end
 
 def subtraction(num1, num2)
+  puts "#{num1} - #{num2}"
+  return num1-num2
 end
 
 def division(num1, num2)
+  puts "#{num1} / #{num2}"
+  return num1/num2
 end
 
 def multiplication(num1, num2)
+  puts "#{num1} * #{num2}"
+  return num1*num2
 end
 
 def modulo(num1, num2)
+  puts "#{num1} % #{num2}"
+  return num1%num2
 end
 
 def square_root(num)
+  puts "#{num}"
+  return Math.sqrt(num)
 end
 
-def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+def order_of_operation(num1=7, num2=43, num3=23, num4=83)
+  puts "#{num1} + ((#{num2}*#{num3}) / #{num4})"
+  return num1 + ((num2*num3) /num4)
 end
